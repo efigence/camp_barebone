ca60ca2b66ba3ba0dc23cb7c23291a1f84c89fa8
Rafał Lisowski <lisukorin@gmail.com>
2015-09-07 12:34:23 +0200

    contact form

---
+++ dst/js/map.js
@@ -0,0 +1,30 @@
+// The latitude and longitude of your business / place
+var position = [52.18471975608178, 21.00073447354123];
+
+function showGoogleMaps() {
+
+    var latLng = new google.maps.LatLng(position[0], position[1]);
+
+    var mapOptions = {
+        zoom: 16, // initialize zoom level - the max value is 21
+        streetViewControl: false, // hide the yellow Street View pegman
+        scaleControl: true, // allow users to zoom the Google Map
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: latLng
+    };
+
+    map = new google.maps.Map(document.getElementById('googlemaps'),
+        mapOptions);
+
+    // Show the default red marker at the location
+    marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    });
+
+    map.panBy($(window).width()/4, 0)
+}
+
+google.maps.event.addDomListener(window, 'load', showGoogleMaps);
